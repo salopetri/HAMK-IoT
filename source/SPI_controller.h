77e3540abfa6178695ac5e452665f89c1decaa89
= <petri.salo@petrimalja.com>
2018-01-24 11:39:41 +0200

    Added SPI-controller header file

---
+++ source/SPI_controller.h
@@ -0,0 +1,23 @@
+/*
+*   THIS IS THE LIBRARY CONTAINING FUNCTIONS FOR SPI COMMUNICATIONS WITH THE SENSORS
+*
+*/
+const unsigned int SPI_Temp_Delay = 10;
+
+uint16_t SPI_Temp_RAW(int MISO) {
+    uint16_t data;
+    uint16_t bit;
+
+    for (int i=0; i<12;i++) {
+        digitalWrite(clock,HIGH);
+        delay(SPI_Temp_Delay);
+        data = data << 1;
+        bit = digitalRead(MISO);
+        data |= bit;
+        Serial.write(bit);
+        digitalWrite(clock, LOW);
+        delay(SPI_Temp_Delay);
+    }
+
+    return data;
+}
