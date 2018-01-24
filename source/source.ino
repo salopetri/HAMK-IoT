a783c10303b2bc26616e535c07708ab53a076876
= <petri.salo@petrimalja.com>
2018-01-24 12:41:53 +0200

    Updates to source code

--- source/source.ino
+++ source/source.ino
@@ -1,16 +1,15 @@
 #include "SPI_controller.h"
 
-const int MISO = 0; // Pin number for MISO
+const int MISOpin = 0; // Pin number for MISO
 const int tempCS = 0; // Pin number for temp. sensors chip select
 const int micCS = 0; // Pin number for mics chip select
 const int humidCS = 0; // Pin number for humidity sensors chip select
 const int CLK = 0; // Pin number for clock 
 
 void setup() {
-  SPI_init(MISO, CLK, tempCS, micCS, humidCS);
+  SPI_init(MISOpin, CLK, tempCS, micCS, humidCS);
 }
 
 void loop() {
-  digitalWrite();
-  SPI_Temp_RAW(MISO, );
+  SPI_Temp_RAW();
 }
