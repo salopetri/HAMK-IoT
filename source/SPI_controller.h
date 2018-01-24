5b8cb5396e59717629f806821872768e49746652
olasirtep <35633173+olasirtep@users.noreply.github.com>
2018-01-24 13:59:56 +0200

    Lab tested actually functioning code

--- source/SPI_controller.h
+++ source/SPI_controller.h
@@ -2,7 +2,7 @@
 *   THIS IS THE LIBRARY CONTAINING FUNCTIONS FOR SPI COMMUNICATIONS WITH THE SENSORS
 *
 */
-const unsigned int _SPI_Temp_Delay = 10;
+const unsigned int _SPI_Temp_Delay = 1;
 int _MISO,_CLK,_tempCS,_micCS,_humidCS;
 
 void SPI_init(int MISOpin, int CLKpin, int tempPin, int micPin, int humidPin) {
@@ -17,22 +17,68 @@ void SPI_init(int MISOpin, int CLKpin, int tempPin, int micPin, int humidPin) {
     pinMode(_micCS, OUTPUT);
     pinMode(_humidCS, OUTPUT);
     pinMode(_CLK, OUTPUT);
+
+    digitalWrite(_tempCS, HIGH);
+    delay(200); // Powerup time for T-sensor
+
+    Serial.begin(9600);
 }
 
 uint16_t SPI_Temp_RAW() {
-    uint16_t data;
+    uint16_t data = 0;
+    uint8_t decimals = 0;
     uint16_t bit;
+    int sign = 1;
 
-    for (int i=0; i<12;i++) {
+    digitalWrite(_tempCS, LOW);
+    delay(100); // Delay for tempsens
+
+    digitalWrite(_CLK,HIGH);
+    delay(_SPI_Temp_Delay);
+    sign = (digitalRead(_MISO) == HIGH) ? -1 : 1;
+    Serial.print(bit);
+    digitalWrite(_CLK, LOW);
+    delay(_SPI_Temp_Delay);
+
+    for (int i=0; i<11;i++) {
         digitalWrite(_CLK,HIGH);
         delay(_SPI_Temp_Delay);
         data = data << 1;
-        bit = (digitalRead(_MISO)) ? 1 : 0;
+        bit = (digitalRead(_MISO) == HIGH) ? 1 : 0;
         data |= bit;
-        Serial.write(bit);
+        Serial.print(bit);
         digitalWrite(_CLK, LOW);
         delay(_SPI_Temp_Delay);
     }
 
+    digitalWrite(_CLK,HIGH);
+    delay(_SPI_Temp_Delay);
+    bit = (digitalRead(_MISO) == HIGH) ? 1 : 0;
+    Serial.print(bit);
+
+    if (bit == 1) decimals += 50;
+        
+    digitalWrite(_CLK, LOW);
+    delay(_SPI_Temp_Delay);
+    
+    digitalWrite(_CLK,HIGH);
+    delay(_SPI_Temp_Delay);
+    bit = (digitalRead(_MISO) == HIGH) ? 1 : 0;
+    Serial.print(bit);
+
+    if (bit == 1) decimals += 25;
+        
+    digitalWrite(_CLK, LOW);
+    delay(_SPI_Temp_Delay);
+
+
+    data *= sign;
+
+    digitalWrite(_tempCS, HIGH);
+    Serial.println();
+    Serial.print(data);
+    Serial.print(".");
+    Serial.print(decimals);
+    Serial.print(" C\n");
     return data;
 }
--- source/source.ino
+++ source/source.ino
@@ -1,10 +1,10 @@
 #include "SPI_controller.h"
 
-const int MISOpin = 0; // Pin number for MISO
-const int tempCS = 0; // Pin number for temp. sensors chip select
-const int micCS = 0; // Pin number for mics chip select
-const int humidCS = 0; // Pin number for humidity sensors chip select
-const int CLK = 0; // Pin number for clock 
+const int MISOpin = 10; // Pin number for MISO
+const int tempCS = 5; // Pin number for temp. sensors chip select
+const int micCS = 4; // Pin number for mics chip select
+const int humidCS = 3; // Pin number for humidity sensors chip select
+const int CLK = 9; // Pin number for clock 
 
 void setup() {
   SPI_init(MISOpin, CLK, tempCS, micCS, humidCS);
